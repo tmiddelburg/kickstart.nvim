aa2a6e30826d6432d58f6ef99a88c920b4a9538b
Thomas Middelburg <tmiddelburg@gmail.com>
2026-08-18 19:14:20 +0200

    feat: basedpyright

--- init.lua
+++ init.lua
@@ -714,17 +714,11 @@ do
   local servers = {
     clangd = {},
     -- gopls = {},
-    pyright = {
+    basedpyright = {
       settings = {
-        pyright = {
+        basedpyright = {
           -- Using Ruff's import organizer
           disableOrganizeImports = true,
-        },
-        python = {
-          analysis = {
-            -- Ignore all files for analysis to exclusively use Ruff for linting
-            ignore = { '*' },
-          },
         },
       },
     },
