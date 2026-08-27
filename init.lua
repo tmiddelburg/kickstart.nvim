91a1c2ab86fe39cca36d20c4c49cadb0883818bc
Thomas Middelburg <tmiddelburg@qblox.com>
2026-08-27 09:49:43 +0200

    feat: do ruff fix and ruff format

--- init.lua
+++ init.lua
@@ -839,7 +839,7 @@ do
     formatters_by_ft = {
       -- rust = { 'rustfmt' },
       -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
+      python = { "ruff_fix", "ruff_format" },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
